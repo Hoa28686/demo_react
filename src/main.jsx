44de01d65ff407325f5b586295e444bdea657d41
Hoa28686 <195274420+Hoa28686@users.noreply.github.com>
2025-03-25 14:40:21 +0200

    add logo, box and footer

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,24 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const name='Hoa';
+const first_page=1997;
+const demo =(
+  <div>
+    <h1>Hi, my name is {name} </h1>
+    <p>I have {2025-first_page} years of building webpages </p>
+  </div>
+)
+const Demo=()=>{
+  return(
+    <div>
+    <h1>Hi, my name is {name} </h1>
+    <p>I have {2025-first_page} years of building webpages </p>
+  </div>
+  )
+}
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App/>
   </StrictMode>,
 )
